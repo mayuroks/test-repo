78a21f499d16c6266e78ed865bc859969d8df637
mayuroks <mayurzenith@gmail.com>
2013-05-05 00:30:38 +0530

    Final version

--- imap-sort.py
+++ imap-sort.py
@@ -1,24 +1,29 @@
-#!/bin/python
-from imapclient import IMAPClient
 import sys,getpass
-user = sys.argv[1:]
+from imapclient import IMAPClient
 
-server = IMAPClient("imap.google.com",ssl=True)
-server.login(user,getpass.getpass())
-server.select_folder("inbox")
-data = server.search("*")
+ser = IMAPClient("imap.gmail.com",use_uid=True,ssl=True)
+try:
+    ser.login("mayuroks","darkchaos")
+except ser.Error, e:
+    print "shit happened", e
+    sys.exit(1)
+ser.select_folder("inbox")
+data = ser.fetch("1:*",['BODY[HEADER.FIELDS (FROM)]'])
 
-data = server.fetch("ALL",['BODY[HEADER.FIELDS (SUBJECT FROM DATE)]'])
-from_key = 'BODY[HEADER.FIELDS (FROM)]'
-
+akey = 'BODY[HEADER.FIELDS (FROM)]'
+string = ""
+output=[]
 for key in data:
-     string = data[key][akey]
-     i1 = string.index("<")
-     i2 = string.index(">")
-     print string[i1+1:i2]
+    string = data[key][akey]
+    if "@" in string:
+        i1 = string.index("@")
+    if ">" in string:
+        i2 = string.index(">")
+    try:
+        output.append(string[i1+1:i2])
+    except TypeError, e:
+        continue
 
-server.logout()
-
-## sort n count -- needs modification
-for count, elem in sorted(((mylist.count(e), e) for e in set(mylist)), reverse=True):
-    print '%s (%d)' % (elem, count)
+for count,elem in sorted(((output.count(e),e) for e in set(output)), reverse=True):
+    print '%s (%d)' % (elem,count)
+ser.logout()
